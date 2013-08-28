b4dac1c038ec0e2d31420a90d832d3b63a0debf1
Jordan Hirst <jehirst15@gmail.com>
2013-08-27 21:18:12 -0700

    read reviews activity

--- cse190_listApp/src/com/example/cse190_listapp/DishDetailsActivity.java
+++ cse190_listApp/src/com/example/cse190_listapp/DishDetailsActivity.java
@@ -3,12 +3,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.restaurant.com.DisplayRestaurantInfo;
 
 import android.app.Activity;
@@ -18,44 +12,34 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.backendWebService.AsyncResponse;
-import com.backendWebService.DownloadWebpageTask;
-//hello
-public class DishDetailsActivity extends Activity implements AsyncResponse {
+public class DishDetailsActivity extends Activity{
 	private Dish selectedDish = null;
 	List<DishPrice> prices = new ArrayList<DishPrice>();
 	List<DishCalories> calories = new ArrayList<DishCalories>();
-	List<Review> reviews = new ArrayList<Review>();
 	String imgUrl  = "";
-	private final static String _getReviewsURL = "https://www.cakesbyannonline.com/cse190/sql_getReviews.php";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		//getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#aa2224")));
 		getActionBar().show();
 		setContentView(R.layout.dish_details);
 		
@@ -75,9 +59,6 @@ public class DishDetailsActivity extends Activity implements AsyncResponse {
 			writeReview.setClickable(false);*/
 		}
 		
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("dishID", selectedDish.getDishId()));
-        initiateDataConnection(_getReviewsURL, params);
         imgUrl = "http://cakesbyannonline.com/cse190/image_dish_lrg/Tomato-Cheese-Pizza.jpg";
         //imgUrl = selectedDish.getPictureLrg();
         new DownloadImageTask((ImageView) findViewById(R.id.dish_picture)).execute(imgUrl);
@@ -108,100 +89,6 @@ public class DishDetailsActivity extends Activity implements AsyncResponse {
 	    protected void onPostExecute(Bitmap result) {
 	        bmImage.setImageBitmap(result);
 	    }
-	}
-	
-	public void initiateDataConnection(String url, List<NameValuePair> params ){
-        ConnectivityManager connMgr = (ConnectivityManager) 
-        getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-			DownloadWebpageTask webtask = new DownloadWebpageTask();
-			webtask.delegate = this;
-        	webtask.setParams(params);
-        	webtask.execute(url);        	
-        } else {
-        	//Create a toast popup
-        	Context context = getApplicationContext();
-        	CharSequence text = "No network connection available.";
-        	int duration = Toast.LENGTH_SHORT;
-        	Toast toast = Toast.makeText(context, text, duration);
-        	toast.show();
-        }
-    }
-	
-	@Override
-	public void processFinish(String result) {
-		// TODO Auto-generated method stub
-		populateReviewList(result);
-		populateListView();
-	}
-
-	private void populateListView() {
-		// TODO Auto-generated method stub
-		@SuppressWarnings({ "unchecked", "rawtypes" })
-		ArrayAdapter adapter = new ArrayAdapter(this,
-				R.layout.display_review, reviews) {
-					@Override
-					public View getView(int position, View convertView, ViewGroup parent) {
-					    View row = convertView;
-					    final int pos = position;
-					    if(row == null){
-					        //getting custom layout for the row
-					        LayoutInflater inflater=getLayoutInflater();
-					        row = inflater.inflate(R.layout.display_review, parent, false);
-					    }
-					    
-					    Review currReview = reviews.get(position);
-					    
-					    TextView dateText = (TextView) row.findViewById(R.id.date_text);
-						dateText.setText(formatDate(currReview.getTimeStamp()));
-						
-						TextView ratingText = (TextView) row.findViewById(R.id.rating_text);
-						ratingText.setText(currReview.getComments());
-					    
-					    RatingBar ratingBar = (RatingBar) row.findViewById(R.id.rating);
-						ratingBar.setRating(currReview.getRating());
-				 
-					    return row; //the row that ListView draws
-					}
-				};
-				
-				ListView reviewsList = (ListView)findViewById(R.id.reviews_list);
-				reviewsList.setAdapter(adapter);
-	}
-
-	protected String formatDate(String timeStamp) {
-		// TODO Auto-generated method stub
-		return timeStamp.substring(5, 10) +  "-" + timeStamp.substring(0, 4);
-	}
-
-	private void populateReviewList(String rawJSON) {
-		// TODO Auto-generated method stub
-		if(rawJSON.contains(new StringBuffer("ratingUserID"))){
-			JSONArray jArry = null;
-			try {
-				jArry = new JSONArray(rawJSON);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			for(int i=0; i<jArry.length(); i++){
-				JSONObject jsonDish = null;
-				try {
-					 jsonDish = jArry.getJSONObject(i);
-					 reviews.add(0, new Review(jsonDish.getString("ratingUserID"),
-							 jsonDish.getDouble("ratingRating"),
-							 jsonDish.getString("ratingComments"),
-							 jsonDish.getString("ratingTimeStamp"),
-							 jsonDish.getString("userImagesURL")
-							 ));
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();					
-				}
-				
-			}
-		}
 	}
 
 	private void populateDishView() {
@@ -339,6 +226,14 @@ public class DishDetailsActivity extends Activity implements AsyncResponse {
 	    	intent.putParcelableArrayListExtra("calories", (ArrayList<? extends Parcelable>) calories);
 			startActivity(intent);
 		}			
+	}
+	
+	/** Called when the user clicks the Read Reviews button */
+	public void readReviewsClick(View view) {
+		Intent intent = new Intent(this, ReadReviewsActivity.class).putExtra("currDish",selectedDish);
+		intent.putParcelableArrayListExtra("prices", (ArrayList<? extends Parcelable>) prices);
+    	intent.putParcelableArrayListExtra("calories", (ArrayList<? extends Parcelable>) calories);
+		startActivity(intent);		
 	}
 	
 	/** Called when the user clicks the Menu button */
---
+++ cse190_listApp/src/com/example/cse190_listapp/ReadReviewsActivity.java
@@ -0,0 +1,203 @@
+package com.example.cse190_listapp;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.restaurant.com.DisplayRestaurantInfo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.backendWebService.AsyncResponse;
+import com.backendWebService.DownloadWebpageTask;
+//hello
+public class ReadReviewsActivity extends Activity implements AsyncResponse {
+	private Dish selectedDish = null;
+	List<DishPrice> prices = new ArrayList<DishPrice>();
+	List<DishCalories> calories = new ArrayList<DishCalories>();
+	List<Review> reviews = new ArrayList<Review>();
+	//String imgUrl  = "";
+	private final static String _getReviewsURL = "https://www.cakesbyannonline.com/cse190/sql_getReviews.php";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		getActionBar().show();
+		setContentView(R.layout.read_reviews);
+		
+		selectedDish = getIntent().getExtras().getParcelable("currDish");
+		prices = getIntent().getExtras().getParcelableArrayList("prices");
+		calories = getIntent().getExtras().getParcelableArrayList("calories");
+		
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("dishID", selectedDish.getDishId()));
+        initiateDataConnection(_getReviewsURL, params);
+        
+        TextView calories = (TextView) findViewById(R.id.reviews_dish_name);
+		calories.setText(selectedDish.getDishName());
+		
+	}
+	
+	public void initiateDataConnection(String url, List<NameValuePair> params ){
+        ConnectivityManager connMgr = (ConnectivityManager) 
+        getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+			DownloadWebpageTask webtask = new DownloadWebpageTask();
+			webtask.delegate = this;
+        	webtask.setParams(params);
+        	webtask.execute(url);        	
+        } else {
+        	//Create a toast popup
+        	Context context = getApplicationContext();
+        	CharSequence text = "No network connection available.";
+        	int duration = Toast.LENGTH_SHORT;
+        	Toast toast = Toast.makeText(context, text, duration);
+        	toast.show();
+        }
+    }
+	
+	@Override
+	public void processFinish(String result) {
+		// TODO Auto-generated method stub
+		populateReviewList(result);
+		populateListView();
+	}
+
+	private void populateListView() {
+		// TODO Auto-generated method stub
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		ArrayAdapter adapter = new ArrayAdapter(this,
+				R.layout.display_review, reviews) {
+					@Override
+					public View getView(int position, View convertView, ViewGroup parent) {
+					    View row = convertView;
+					    final int pos = position;
+					    if(row == null){
+					        //getting custom layout for the row
+					        LayoutInflater inflater=getLayoutInflater();
+					        row = inflater.inflate(R.layout.display_review, parent, false);
+					    }
+					    
+					    Review currReview = reviews.get(position);
+					    
+					    TextView dateText = (TextView) row.findViewById(R.id.date_text);
+						dateText.setText(formatDate(currReview.getTimeStamp()));
+						
+						TextView ratingText = (TextView) row.findViewById(R.id.rating_text);
+						ratingText.setText(currReview.getComments());
+					    
+					    RatingBar ratingBar = (RatingBar) row.findViewById(R.id.rating);
+						ratingBar.setRating(currReview.getRating());
+				 
+					    return row; //the row that ListView draws
+					}
+				};
+				
+				ListView reviewsList = (ListView)findViewById(R.id.reviews_list);
+				reviewsList.setAdapter(adapter);
+	}
+
+	protected String formatDate(String timeStamp) {
+		// TODO Auto-generated method stub
+		return timeStamp.substring(5, 10) +  "-" + timeStamp.substring(0, 4);
+	}
+
+	private void populateReviewList(String rawJSON) {
+		// TODO Auto-generated method stub
+		if(rawJSON.contains(new StringBuffer("ratingUserID"))){
+			JSONArray jArry = null;
+			try {
+				jArry = new JSONArray(rawJSON);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			for(int i=0; i<jArry.length(); i++){
+				JSONObject jsonDish = null;
+				try {
+					 jsonDish = jArry.getJSONObject(i);
+					 reviews.add(0, new Review(jsonDish.getString("ratingUserID"),
+							 jsonDish.getDouble("ratingRating"),
+							 jsonDish.getString("ratingComments"),
+							 jsonDish.getString("ratingTimeStamp"),
+							 jsonDish.getString("userImagesURL")
+							 ));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();					
+				}
+				
+			}
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		String username = preferences.getString("isLoggedIn", "false");
+		if(username.equals("false"))
+		{
+			getMenuInflater().inflate(R.menu.logged_out, menu);
+		}
+		else{		// 
+			getMenuInflater().inflate(R.menu.logged_in, menu);
+		}
+		return true;
+		//setVlauesToTextView(R.id.hello_id,"abcd");
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.action_edit_profile:
+	        	Intent intent = new Intent(getApplicationContext(), EditProfileActivity.class);
+	    		startActivity(intent);
+	            return true;
+	        case R.id.action_log_out:
+	        	SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+	        	SharedPreferences.Editor editor = preferences.edit();
+				editor.putString("isLoggedIn", "false");
+				editor.commit();
+				Intent intent2 = new Intent(getApplicationContext(), DisplayDishesActivity.class);
+	    		startActivity(intent2);
+	            return true;
+	        case R.id.action_log_in_sign_up:
+	        	Intent intent3 = new Intent(getApplicationContext(), FirstLaunchActivity.class);
+	    		startActivity(intent3);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+
+}
